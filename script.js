dc27654dff0d9e3f9556e112195672f7b6af3e86
Kuma-desu <rifkysyachhalim2001@gmail.com>
2025-06-07 23:18:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Prefill data dengan JavaScript
+document.getElementById("nama").value = "Rifky Syach Halim";
+document.getElementById("usia").value = 23;
+document.getElementById("gender").value = "Laki-laki";
+document.getElementById("tinggi").value = 160 ; // sekitar segitu aku lupa ukuran lagi
+document.getElementById("berat").value = 55; // sekitar segitu belum diupdate lagi wkkw
+document.getElementById("suku").value = "Sunda";
+document.getElementById("stack").value = "JavaScript, Python, PHP, Csherp, C, C++";
+document.getElementById("zodiak").value = "Cancer";
+document.querySelector('input[name="status"][value="Kuliah"]').checked = true;
+
+document.getElementById("lagu").value = `1. Pierce The Veil - A Match Into Water
+2. Jimin - Like Crazy
+3. ScimmiaSka - With You
+4. The Rose - Back To Me
+5. LE SSERAFIM - HOT`;
+
+document.getElementById("fakta").value = `1. Ngopi lebih dari 3x sehari
+2. Sekarang lagi makan dikit (karena makanannya ga enak), tapi kalau makanan yang enak kayak bakso atau mie, dan ramen beh suka banget semangat banget.
+3. Pernah ikut bootcamp/Pelatihan cuma nggak datang
+4. Suka kebanyakan menghayal wkwk (biasanya kayak gitu suka tiba-tiba ada inspirasi ataupun motivasi untuk ngerjain sesewaktu yang)
+5. Lebih suka yang manis, soalnya kalau pahit ga nyaman banget. Contohnya senyum kamu manis banget kayak perment wkwk`;
+
+document.querySelector('input[name="nickname"][value="YES"]').checked = true;
+document.querySelector('input[name="style"][value="YES"]').checked = true;
+document.querySelector('input[name="clingy"][value="YES"]').checked = true;
+
+document.getElementById("instagram").value = "@rifkysyhlm_";
+document.getElementById("facebook").value = "Rifky Syach Halim";
+document.getElementById("github").value = "https://github.com/Kuma-desu";
+document.getElementById("linkedin").value = "https://www.linkedin.com/in/rifky-syach-halim-564a96297/";
+document.getElementById("wa").value = "089513248503";
+document.getElementById("preview").src = "img/img.jpg";
+
